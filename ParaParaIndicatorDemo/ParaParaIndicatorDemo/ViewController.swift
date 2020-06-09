d70a827654bdb423f8247b1c415db931e1549849
Daisuke TONOSAKI <daisuke.t.jp@gmail.com>
2020-06-09 22:26:04 +0900

    Add workspace

---
+++ ParaParaIndicatorDemo/ParaParaIndicatorDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParaParaIndicatorDemo
+//
+//  Created by Daisuke TONOSAKI on 2020/06/09.
+//  Copyright © 2020 Daisuke TONOSAKI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
