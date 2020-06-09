15d886008a6b61bbc0c7342ab51f6de00dffc397
Daisuke TONOSAKI <daisuke.t.jp@gmail.com>
2020-06-09 19:10:07 +0900

    Add empty project

---
+++ ParaParaIndicator/ParaParaIndicator/ParaParaIndicator.h
@@ -0,0 +1,19 @@
+//
+//  ParaParaIndicator.h
+//  ParaParaIndicator
+//
+//  Created by Daisuke TONOSAKI on 2020/06/09.
+//  Copyright © 2020 Daisuke TONOSAKI. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ParaParaIndicator.
+FOUNDATION_EXPORT double ParaParaIndicatorVersionNumber;
+
+//! Project version string for ParaParaIndicator.
+FOUNDATION_EXPORT const unsigned char ParaParaIndicatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ParaParaIndicator/PublicHeader.h>
+
+
